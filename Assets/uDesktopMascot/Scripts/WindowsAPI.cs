dedbe3b3fed108925642f377894f83338d9f63e7
yousan <ka1357amnbpdr@gmail.com>
2025-01-13 04:17:16 +0900

    fix: IL2CPPの対応

--- Assets/uDesktopMascot/Scripts/WindowsAPI.cs
+++ Assets/uDesktopMascot/Scripts/WindowsAPI.cs
@@ -5,29 +5,25 @@ using System.Text;
 
 namespace uDesktopMascot
 {
-
-    /// <summary>
-    /// Windows API
-    /// </summary>
-    public class WindowsAPI
+    public static class WindowsAPI
     {
+        // デリゲートの宣言
         public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 
+        // EnumWindows 関数の宣言
         [DllImport("user32.dll")]
-        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+        public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-
-        [DllImport("user32.dll", SetLastError = true)]
+        [DllImport("user32.dll")]
         public static extern bool IsWindowVisible(IntPtr hWnd);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
 
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        // RECT 構造体の定義
         [StructLayout(LayoutKind.Sequential)]
         public struct RECT
         {
