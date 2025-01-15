5f710c8b81db81ef126577af4ff017668de710cc
yousan <ka1357amnbpdr@gmail.com>
2025-01-15 21:56:43 +0900

    fix: 品質判定の処理の修正

--- Assets/uDesktopMascot/Scripts/QualityLevelAdjuster.cs
+++ Assets/uDesktopMascot/Scripts/QualityLevelAdjuster.cs
@@ -7,6 +7,7 @@ namespace uDesktopMascot
     ///     品質レベルを調整するクラス
     /// </summary>
     public static class QualityLevelAdjuster
+    
     {
         /// <summary>
         ///     品質レベルを調整
@@ -14,22 +15,29 @@ namespace uDesktopMascot
         /// <returns></returns>
         public static int AdjustQualityLevel()
         {
-            var qualityLevel = 0; // デフォルトの品質レベルを最低に設定
+            int qualityLevel = 0; // デフォルトの品質レベルを最低に設定
+
+            // デバイス情報の取得とデフォルト値の設定
+            int processorFrequency = SystemInfo.processorFrequency > 0 ? SystemInfo.processorFrequency : 2000; // MHz
+            int processorCount = SystemInfo.processorCount > 0 ? SystemInfo.processorCount : 2;
+            int systemMemorySize = SystemInfo.systemMemorySize > 0 ? SystemInfo.systemMemorySize : 4000; // MB
+            int graphicsMemorySize = SystemInfo.graphicsMemorySize > 0 ? SystemInfo.graphicsMemorySize : 512; // MB
+            int graphicsShaderLevel = SystemInfo.graphicsShaderLevel > 0 ? SystemInfo.graphicsShaderLevel : 30;
 
             // CPUの評価
-            if (SystemInfo.processorFrequency >= 3000 && SystemInfo.processorCount >= 4)
+            if (processorFrequency >= 3000 && processorCount >= 4)
             {
                 qualityLevel += 1;
             }
 
             // メモリの評価
-            if (SystemInfo.systemMemorySize >= 8000)
+            if (systemMemorySize >= 8000)
             {
                 qualityLevel += 1;
             }
 
             // GPUの評価
-            if (SystemInfo.graphicsMemorySize >= 2048 && SystemInfo.graphicsShaderLevel >= 35)
+            if (graphicsMemorySize >= 2048 && graphicsShaderLevel >= 35)
             {
                 qualityLevel += 1;
             }
