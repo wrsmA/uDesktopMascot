07e0cd8dd452bbd78fc3a35f9f76c05c3f0f3dad
yousan <ka1357amnbpdr@gmail.com>
2025-01-19 22:30:29 +0900

    feat: BGMControllerのインスペクターにbgm audioClipを最新の状態で再登録するボタンの追加

---
+++ Assets/uDesktopMascot/Scripts/Manager/BGMController.Editor.cs
@@ -0,0 +1,25 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+namespace uDesktopMascot
+{
+    /// <summary>
+    ///    BGMを制御するクラス Editor拡張
+    /// </summary>
+    public partial class BGMController
+    {
+        [Button("BGMファイル登録を最新化")]
+        private void UpdateBGMFiles()
+        {
+            bgmClips.Clear();
+            var bgmFiles = Resources.LoadAll<AudioClip>("DefaultBGM");
+            foreach (var bgm in bgmFiles)
+            {
+                bgmClips.Add(bgm);
+            }
+        }
+    }
+}
+#endif
