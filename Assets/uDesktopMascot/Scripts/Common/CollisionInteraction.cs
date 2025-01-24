7a04b719c1cbec1951c933cf1eec18b5741ed466
wrsmA <kubo.taiga@alleystudio-jp.com>
2025-01-25 06:35:20 +0900

    インタラクション「撫で」の実装

--- Assets/uDesktopMascot/Scripts/Common/CollisionInteraction.cs
+++ Assets/uDesktopMascot/Scripts/Common/CollisionInteraction.cs
@@ -12,6 +12,10 @@ namespace uDesktopMascot
         public void OnDrag(InputAction.CallbackContext context)
         {
             // なでる動作かどうかを判定
+            if (context.started)
+            {
+                _nadeTime = 0.0f;
+            }
             if (context.performed)
             {
                 _nadeTime += Time.deltaTime;
@@ -20,17 +24,16 @@ namespace uDesktopMascot
             {
                 _nadeTime = 0.0f;
             }
-            Log.Info("なでる時間: " + _nadeTime);
+            if (_nadeTime > 1.0f)
+            {
+                Log.Info("なでなで");
+                _nadeTime = 0.0f;
+            }
         }
 
-        public void OnTriggerEnter2D(Collider2D collision)
+        public void ResetNadeTime()
         {
-            Log.Info("Enter2D");
-        }
-
-        public void OnTriggerExit2D(Collider2D collision)
-        {
-            Log.Info("Exit2D");
+            _nadeTime = 0.0f;
         }
     }
 }
--- Assets/uDesktopMascot/Scripts/Scene/MainScene/MainSceneView.cs
+++ Assets/uDesktopMascot/Scripts/Scene/MainScene/MainSceneView.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Unity.Logging;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace uDesktopMascot
 {
@@ -24,6 +27,8 @@ namespace uDesktopMascot
         /// </summary>
         private List<CharacterControllerBase> _characters = new(Const.MaxCharacterCount);
 
+        private RaycastHit2D[] _hits = new RaycastHit2D[Const.MaxCharacterCount];
+
         public event Action<string, EModelType> ModelFileSelected;
         public event Action LoadExistsButtonClicked;
         public event Action<int> ExistModelSelected;
@@ -33,7 +38,7 @@ namespace uDesktopMascot
             _contextMenu.FileSelected += (path, type) => ModelFileSelected?.Invoke(path, type);
             _contextMenu.LoadExistsButtonClicked += () => LoadExistsButtonClicked?.Invoke();
             _contextMenu.gameObject.SetActive(false);
-            
+
             _existModelsMenu.ModelSelected += i => ExistModelSelected?.Invoke(i);
             _existModelsMenu.gameObject.SetActive(false);
         }
@@ -56,10 +61,15 @@ namespace uDesktopMascot
             }
         }
 
+        /// <summary>
+        /// 表示するキャラクターを追加する
+        /// </summary>
+        /// <param name="character"></param>
         public void AddCharacter(CharacterControllerBase character)
         {
             // コンテキストメニューを閉じる
             _contextMenu.gameObject.SetActive(false);
+            _existModelsMenu.gameObject.SetActive(false);
 
             // NOTE: 現状は1体だけ表示するので全部消す
             RemoveAllCharacters();
@@ -68,6 +78,10 @@ namespace uDesktopMascot
             character.Appear();
         }
 
+        /// <summary>
+        /// キャラクターを削除する
+        /// </summary>
+        /// <param name="character"></param>
         public void RemoveCharacter(CharacterControllerBase character)
         {
             character.Disappear();
@@ -81,6 +95,49 @@ namespace uDesktopMascot
                 character.Disappear();
             }
             _characters.Clear();
+        }
+
+        // TODO: インタラクション系の処理は独立モジュールにする
+        private CollisionInteraction[] _lastInteraction = new CollisionInteraction[Const.MaxCharacterCount];
+
+        /// <summary>
+        /// ドラッグを処理する
+        /// </summary>
+        /// <param name="context"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void ProcessDrag(InputAction.CallbackContext context)
+        {
+            // マウスのスクリーン座標をワールド座標に変換
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+            // Raycastを発射
+            var hitCount = Physics2D.RaycastNonAlloc(mousePosition, Vector2.zero, _hits, 0f, LayerMask.GetMask("Default"));
+
+            // デバッグ用のRayをシーンビューに表示（マウス位置）
+            Debug.DrawRay(mousePosition, Vector2.up * 0.1f, Color.green);
+
+            if (hitCount > 0)
+            {
+                for (var i = 0; i < hitCount; ++i)
+                {
+                    if (_hits[i].collider.gameObject.TryGetComponent<CollisionInteraction>(out var interaction))
+                    {
+                        interaction.OnDrag(context);
+                        _lastInteraction[i] = interaction;
+                    }
+                }
+            } 
+            else
+            {
+                for (var i = 0; i < Const.MaxCharacterCount; ++i)
+                {
+                    if (_lastInteraction[i] != null)
+                    {
+                        _lastInteraction[i].ResetNadeTime();
+                        _lastInteraction[i] = null;
+                    }
+                }
+            }
         }
     }
 }
