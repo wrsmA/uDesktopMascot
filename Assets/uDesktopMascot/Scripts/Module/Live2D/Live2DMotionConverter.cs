22ad12c107eb13ac98e66867ff822768141cb181
tsumugixenia <tsumugi.xenia@gmail.com>
2025-02-01 12:06:43 +0900

    一応モーションつけれた

--- Assets/uDesktopMascot/Scripts/Module/Live2D/Live2DMotionConverter.cs
+++ Assets/uDesktopMascot/Scripts/Module/Live2D/Live2DMotionConverter.cs
@@ -13,6 +13,8 @@ using UnityEditor;
 using Live2D.Cubism.Core;
 using Live2D.Cubism.Framework.Json;
 using Live2D.Cubism.Framework.MotionFade;
+using Live2D.Cubism.Framework.Motion;
+using Live2D.Cubism.Framework.MotionFade;
 using Live2D.Cubism.Framework.Pose;
 #endif // ENABLE_LIVE2D
 
@@ -23,39 +25,57 @@ namespace uDesktopMascot.Live2D
     #if ENABLE_LIVE2D
     public class Live2DMotionConverter
     {
-        public static async UniTask<Animation> AttachIdleMotion(string jsonPath,GameObject model, CubismModel3Json model3Json)
+        public static async UniTask<Animator> AttachIdleMotion(string jsonPath,GameObject model, CubismModel3Json model3Json)
         {
             if (!File.Exists(jsonPath))
             {
                 Log.Error($"File not found: {jsonPath}");
                 return null;
-
             }
 
-            var jsonFolderPath = Path.GetDirectoryName(jsonPath);
-            var vtubeJsonFilePath = FileUtility.SearchFileWithWildcard(jsonFolderPath, "*.vtube.json");
+            var model3JsonMotions = model3Json.FileReferences.Motions;
+            Log.Info($"model3JsonMotions: {model3JsonMotions}");
+            var motionNames = model3JsonMotions.GroupNames;
+            Log.Info($"motionNames: {motionNames}");
 
-            if (string.IsNullOrEmpty(vtubeJsonFilePath))
-            {
-                Log.Error($"File not found: {jsonFolderPath}/*.vtube.json");
-                return null;
-
-            }
-
-            var vtubeJsonFile = File.ReadAllText(vtubeJsonFilePath);
-            var vtubeJsonStr = vtubeJsonFile.Replace("\n", "").Replace("\r", "");
-            var vtubeJson = JsonUtility.FromJson<VtubeJson> (vtubeJsonStr);
-
-            if (vtubeJson == null)
-            {
-                Log.Error($"Failed to parse json: {vtubeJsonFilePath}");
-                return null;
-
-            }
-
+            var idleMotion = "";
             var pose3json = model3Json.Pose3Json;
+            var jsonFolderPath = Path.GetDirectoryName(jsonPath);
 
-            var idleMotionPath = Path.Combine(jsonFolderPath, vtubeJson.FileReferences.IdleAnimation);
+            if (motionNames.Length > 0)
+            {
+                var idleMotionIndex = Array.IndexOf(motionNames, "Idle");
+                var idleMotionName = motionNames[idleMotionIndex];
+                var idleMotions = model3JsonMotions.Motions[idleMotionIndex];
+                idleMotion = idleMotions[0].File;
+            }
+            else
+            {
+                Log.Info($"idleMotion: {idleMotion}");
+                var vtubeJsonFilePath = FileUtility.SearchFileWithWildcard(jsonFolderPath, "*.vtube.json");
+
+                if (string.IsNullOrEmpty(vtubeJsonFilePath))
+                {
+                    Log.Error($"File not found: {jsonFolderPath}/*.vtube.json");
+                    return null;
+
+                }
+
+                var vtubeJsonFile = File.ReadAllText(vtubeJsonFilePath);
+                var vtubeJsonStr = vtubeJsonFile.Replace("\n", "").Replace("\r", "");
+                var vtubeJson = JsonUtility.FromJson<VtubeJson> (vtubeJsonStr);
+
+                if (vtubeJson == null)
+                {
+                    Log.Error($"Failed to parse json: {vtubeJsonFilePath}");
+                    return null;
+
+                }
+                idleMotion = vtubeJson.FileReferences.IdleAnimation;
+            }
+
+
+            var idleMotionPath = Path.Combine(jsonFolderPath, idleMotion);
             Log.Info($"idleMotionPath: {idleMotionPath}");
 
             var idleMotionJsonStr = File.ReadAllText(idleMotionPath);
@@ -78,27 +98,45 @@ namespace uDesktopMascot.Live2D
 
             animationClip.name = "Idle";
 
-            var animation = model.GetComponent<Animation>();
-            if (animation == null)
+            var cubismFadeMotionData = CubismFadeMotionData.CreateInstance(idleMotionJson,"Idle",animationClip.length, false, true);
+            AssetDatabase.CreateAsset(cubismFadeMotionData, "Assets/Idle.fade.asset");
+
+            var cubismFadeMotionDataInstanceId = cubismFadeMotionData.GetInstanceID();
+
+            var fadeMotions = ScriptableObject.CreateInstance<CubismFadeMotionList>();
+            fadeMotions.MotionInstanceIds = new int[0];
+            fadeMotions.CubismFadeMotionObjects = new CubismFadeMotionData[0];
+            AssetDatabase.CreateAsset(fadeMotions, "Assets/Idle.fadeMotionList.asset");
+
+            var motionIndex = fadeMotions.CubismFadeMotionObjects.Length;
+            Array.Resize(ref fadeMotions.MotionInstanceIds, motionIndex+1);
+            fadeMotions.MotionInstanceIds[motionIndex] = cubismFadeMotionDataInstanceId;
+            Array.Resize(ref fadeMotions.CubismFadeMotionObjects, motionIndex+1);
+            fadeMotions.CubismFadeMotionObjects[motionIndex] = cubismFadeMotionData;
+
+            var animator = model.GetComponent<Animator>();
+            if (animator == null)
             {
-                animation = model.AddComponent<Animation>();
+                animator = model.AddComponent<Animator>();
             }
 
-            animation.AddClip(animationClip, animationClip.name);
-            animation.clip = animationClip;
-            animation.playAutomatically = true;
+            var cubisumFadeController = model.GetComponent<CubismFadeController>();
+            if (cubisumFadeController == null)
+            {
+                cubisumFadeController = model.AddComponent<CubismFadeController>();
+            }
 
+            cubisumFadeController.CubismFadeMotionList = fadeMotions;
 
-            animation.Play("Idle");         
+            var cubismMotionController = model.GetComponent<CubismMotionController>();
+            if (cubismMotionController == null)
+            {
+                cubismMotionController = model.AddComponent<CubismMotionController>();
+            }
 
-
-            return animation;
+            cubismMotionController.PlayAnimation(animationClip, isLoop: true);     
+            return animator;
         }
-
-        
-
-
-        
     }
 
     [System.Serializable]
